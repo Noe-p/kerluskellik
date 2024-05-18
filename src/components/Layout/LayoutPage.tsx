5edf5bbf8ea6e3944ad8459dcefd4f581f205d6c
Noé <noe.philippe@bob-le-developpeur.com>
2024-05-18 17:13:17 +0200

    ✨ Add google analytics

--- src/components/Layout/LayoutPage.tsx
+++ src/components/Layout/LayoutPage.tsx
@@ -1,7 +1,9 @@
 import { SeoHead } from '@/container/components';
+import { pageview } from '@/services/analytics';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import { ReactNode, useEffect } from 'react';
 
 interface LayoutPageProps {
   children?: ReactNode;
@@ -9,6 +11,18 @@ interface LayoutPageProps {
 
 export function LayoutPage(props: LayoutPageProps): JSX.Element {
   const { children } = props;
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
